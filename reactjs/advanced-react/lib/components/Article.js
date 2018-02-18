e51e61d91bb4733f0aeaef6706dfa4f2cbb49229
Dmitriy Dubson <d.dubson@gmail.com>
2018-02-18 18:22:03 -0500

    advanced-react: displaying sample data on screen

---
+++ reactjs/advanced-react/lib/components/Article.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Article = props => {
+  const {article, author} = props;
+  return (
+    <div>
+      <div>{article.title}</div>
+      <div>{article.date}</div>
+      <div><a href={author.website}>{author.firstName} {author.lastName}</a></div>
+      <div>{article.body}</div>
+    </div>
+  )
+};
---
+++ reactjs/advanced-react/lib/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Article } from "./Article";
+
+export const ArticleList = (props) => (
+  <div>
+    {Object.values(props.articles).map(article => {
+      return <Article
+        key={article.id}
+        article={article}
+        author={props.authors[article.authorId]}
+      />
+    })}
+  </div>
+);
